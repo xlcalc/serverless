4ce1c406cdd5bb8eb470e8877e92b80d1da8c865
xlcalc <121704822+xlcalc@users.noreply.github.com>
2023-06-09 14:48:10 +0300

    Create expand-url.js

---
+++ netlify/functions/expand-url.js
@@ -0,0 +1,8 @@
+export const handler = async (event, context) => {
+//  const response = event.body; // echo
+  const response = await fetch("https://n9.cl/dsao0").then(response => response.json());
+  return {
+    statusCode: 200,
+    body: JSON.stringify(response)
+  }
+}
