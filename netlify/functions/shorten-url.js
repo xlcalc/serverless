2a4fcb993e92ca8f7843d30ba32f5aca40bd027f
xlcalc <121704822+xlcalc@users.noreply.github.com>
2023-06-09 10:12:27 +0300

    Update shorten-url.js
    send echo

--- netlify/functions/shorten-url.js
+++ netlify/functions/shorten-url.js
@@ -3,7 +3,7 @@ export const handler = async (event, context) => {
   const data = JSON.parse(event.body);
   return {
     statusCode: 200,
-    body: JSON.stringify(data), //echo data
+    body: JSON.stringify({test: "echo"}), //echo data
   }
 }
 
