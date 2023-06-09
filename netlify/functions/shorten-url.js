f3d257469c94bf1d6d108ed2f675671a871828f3
xlcalc <121704822+xlcalc@users.noreply.github.com>
2023-06-09 10:44:09 +0300

    Update shorten-url.js
    call n9 api

--- netlify/functions/shorten-url.js
+++ netlify/functions/shorten-url.js
@@ -1,9 +1,22 @@
 export const handler = async (event, context) => {
   //const key = process.env.TEST_KEY;
-  const data = JSON.parse(event.body);
+  //const data = JSON.parse(event.body);
+  const response = await callN9(event);
   return {
     statusCode: 200,
-    body: JSON.stringify(data), //echo data
+    body: JSON.stringify(response)
   }
 }
 
+    const callN9 = async (event) => {
+        const data = JSON.parse(event.body);
+        const response = await fetch('https://n9.cl/api/short', {
+	//const response = await fetch('/.netlify/functions/test', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        }).then(response => response.json());
+	    return response;
+    }
