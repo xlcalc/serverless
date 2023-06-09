43acde96275593ea8bb4d483de06822a3591a08f
xlcalc <121704822+xlcalc@users.noreply.github.com>
2023-06-09 14:50:42 +0300

    Update expand-url.js
    try echo event.body

--- netlify/functions/expand-url.js
+++ netlify/functions/expand-url.js
@@ -1,6 +1,6 @@
 export const handler = async (event, context) => {
-//  const response = event.body; // echo
-  const response = await fetch("https://n9.cl/dsao0").then(response => response.json());
+  const response = event.body; // echo
+//  const response = await fetch("https://n9.cl/dsao0").then(response => response.json());
   return {
     statusCode: 200,
     body: JSON.stringify(response)
