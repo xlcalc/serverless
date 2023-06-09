0bc3fcc3875c7c68dbd9a9416cbb1665bc4fc794
xlcalc <121704822+xlcalc@users.noreply.github.com>
2023-06-09 11:24:28 +0300

    Update shorten-url.js
    restored JSON parse/stringify in callN9

--- netlify/functions/shorten-url.js
+++ netlify/functions/shorten-url.js
@@ -7,14 +7,14 @@ export const handler = async (event, context) => {
 }
 
     const callN9 = async (event) => {
-//        const data = JSON.parse(event.body);
+        const data = JSON.parse(event.body);
         const response = await fetch('https://n9.cl/api/short', {
           method: 'POST',
           headers: {
               'Content-Type': 'application/json'
           },
-//          body: JSON.stringify(data)
-          body: event.body
+          body: JSON.stringify(data)
+//          body: event.body
         }).then(response => response.json());
 	    return response;
     }
