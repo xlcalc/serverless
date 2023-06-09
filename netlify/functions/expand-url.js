286a9b21d8afc39970256308e9505622a7131ae1
xlcalc <121704822+xlcalc@users.noreply.github.com>
2023-06-09 18:04:25 +0300

    Update expand-url.js
    try fetch result as text

--- netlify/functions/expand-url.js
+++ netlify/functions/expand-url.js
@@ -1,10 +1,11 @@
 export const handler = async (event, context) => {
 //  const response = event.body; // echo
-//  const response = await fetch("https://n9.cl/dsao0").then(response => response.text());
-    const response = await fetch("https://n9.cl/dsao0");
+  const response = await fetch("https://n9.cl/dsao0").then(response => response.text());
+//    const response = await fetch("https://n9.cl/dsao0");
   return {
     statusCode: 200,
-    body: JSON.stringify(response.url)
+//    body: JSON.stringify(response.url)
+    body: JSON.stringify(response)
 //    body: response.url
   }
 }
